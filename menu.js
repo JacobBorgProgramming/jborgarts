f6301df1f5927d3622b7083e45f27dc842d7288b
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-01 17:22:36 -0700

    new site

---
+++ menu.js
@@ -0,0 +1,11 @@
+function menu(){
+    document.getElementById("leftnav").innerHTML = ' <a href="./index.html"> <img src="./img/jb_new.png" width="60px"> </a> '
+
+
+    document.getElementById("rightnav").innerHTML = ' <a href="./art.html" ><img id="iconEffectRight" src="./icons/paint-kit-dynamic-color.png" width="60px"></a> <a href="./photography.html" ><img id="iconEffect" src="./icons/camera-dynamic-color.png" width="60px"></a> <a href="./development.html"><img id="iconEffectRight" src="./icons/computer-dynamic-color.png" width="60px"></a> <a href="https://cloud.borgcreative.com/my/index.php/apps/forms/zfJ4x8rztidJCyGA" target="_blank"><img id="iconEffect" src="./icons/mail-dynamic-color.png" width="60px"></a> '
+
+
+
+    document.getElementById("foot1").innerHTML = '<a href="https://twitter.com/jborg_arts" target="_blank"><img id="iconEffect" src="./icons/twitter.png" width="80px"></a> <a href="https://www.linkedin.com/in/jacob-borg-94376b196/" target="_blank"><img id="iconEffectRight" src="./icons/linkedin.png" width="80px"></a>  <a href="https://www.instagram.com/jborg_arts/" target="_blank"> <img id="iconEffect" src="./icons/instagram.png" width="80px"> </a> '
+
+}
