555a66712fbf97f67e08e9fd2eeed8731375d184
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-04-15 10:19:45 -0700

    update

--- menu.js
+++ menu.js
@@ -2,10 +2,10 @@ function menu(){
     document.getElementById("leftnav").innerHTML = ' <a href="./index.html"> <img id="iconEffect"  src="./img/jb_new.png" width="50px"> </a> '
 
 
-    document.getElementById("rightnav").innerHTML = ' <a href="./art.html" ><img id="iconEffectRight" src="./icons/paint-kit-dynamic-color.png" width="50px"></a> <a href="./photography.html" ><img id="iconEffect" src="./icons/camera-dynamic-color.png" width="50px"></a> <a href="./development.html"><img id="iconEffectRight" src="./icons/computer-dynamic-color.png" width="50px"></a> <a href="https://cloud.borgcreative.com/my/index.php/apps/forms/zfJ4x8rztidJCyGA" target="_blank"><img id="iconEffect" src="./icons/mail-dynamic-color.png" width="50px"></a> '
+    document.getElementById("rightnav").innerHTML = ' <a href="./art.html" ><img id="iconEffectRight" src="./icons/paintkit_line.png" width="50px"></a> <a href="./photography.html" ><img id="iconEffect" src="./icons/camera_line.png" width="50px"></a> <a href="./development.html"><img id="iconEffectRight" src="./icons/computer_line.png" width="50px"></a> <a href="https://cloud.borgcreative.com/my/index.php/apps/forms/zfJ4x8rztidJCyGA" target="_blank"><img id="iconEffect" src="./icons/mail_line.png" width="50px"></a> '
 
 
 
-    document.getElementById("foot1").innerHTML = '<a href="https://twitter.com/jborg_arts" target="_blank"><img id="iconEffect" src="./icons/twitter.png" width="80px"></a> <a href="https://www.linkedin.com/in/jacob-borg-94376b196/" target="_blank"><img id="iconEffectRight" src="./icons/linkedin.png" width="80px"></a>  <a href="https://www.instagram.com/jborg_arts/" target="_blank"> <img id="iconEffect" src="./icons/instagram.png" width="80px"> </a> '
+    document.getElementById("foot1").innerHTML = '<a href="https://twitter.com/jborg_arts" target="_blank"><img id="iconEffect" src="./icons/twitter_line.png" width="80px"></a> <a href="https://www.linkedin.com/in/jacob-borg-94376b196/" target="_blank"><img id="iconEffectRight" src="./icons/linkedin_line.png" width="80px"></a>  <a href="https://www.instagram.com/jborg_arts/" target="_blank"> <img id="iconEffect" src="./icons/instagram_line.png" width="80px"> </a> <a rel="me" target="_blank" href="https://social.librem.one/@jborgarts"><img id="iconEffect" src="./icons/libremsocial.png" width="80px"></a>'
 
 }
