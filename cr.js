51a43f115412d4ea222fca0486511635dd1e393d
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-06 11:50:18 -0700

    Create cr.js

---
+++ cr.js
@@ -0,0 +1,5 @@
+function jborgarts(){
+    /* Top most entry */
+    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/jb_bauhaus_blues.png'
+    document.getElementById("jborgarts_link").href = 'https://jborgarts.com/'
+}
