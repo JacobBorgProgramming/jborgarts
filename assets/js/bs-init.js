040d6cb312484fed96a30aa97973ebb3e0acb553
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2022-12-11 15:16:01 -0700

    update

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -93,10 +93,4 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 
 })();
-
-	var charts = document.querySelectorAll('[data-bss-chart]');
-
-	for (var chart of charts) {
-		chart.chart = new Chart(chart, JSON.parse(chart.dataset.bssChart));
-	}
 }, false);
