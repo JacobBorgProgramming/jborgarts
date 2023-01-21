84f2da648cc37baaaf55be9fa142cff4ec07f1f7
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-01-20 17:01:01 -0700

    update
    -New Design
    -BauHaus Overhaul

--- cr.js
+++ cr.js
@@ -1,11 +1,11 @@
 function jborgarts(){
     /* Top most entry */
-    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/JB_raven.png'
+    document.getElementById("jborgarts").src = 'https://jborgarts.com/branding/jb_bauhaus_blues.png'
     document.getElementById("jborgarts_link").href = 'https://jborgarts.com/'
 }
 
 function jborgartslight(){
     /* Top most entry */
-    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/JB_raven_dark.png'
+    document.getElementById("jborgarts").src = 'https://jborgarts.com/branding/jb_bauhaus_blues.png'
     document.getElementById("jborgarts_link").href = 'https://jborgarts.com/'
 }
