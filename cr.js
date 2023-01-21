e58b39e717233785454a659639315e77e4d66e80
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2023-01-20 17:07:36 -0700

    Update cr.js

--- cr.js
+++ cr.js
@@ -1,11 +1,11 @@
 function jborgarts(){
     /* Top most entry */
-    document.getElementById("jborgarts").src = 'https://jborgarts.com/branding/jb_bauhaus_blues.png'
+    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/branding/jb_bauhaus_blues.png'
     document.getElementById("jborgarts_link").href = 'https://jborgarts.com/'
 }
 
 function jborgartslight(){
     /* Top most entry */
-    document.getElementById("jborgarts").src = 'https://jborgarts.com/branding/jb_bauhaus_blues.png'
+    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/branding/jb_bauhaus_blues.png'
     document.getElementById("jborgarts_link").href = 'https://jborgarts.com/'
 }
