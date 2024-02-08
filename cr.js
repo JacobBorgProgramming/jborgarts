f6af0727c42d2569a7f9b15d6b21642710e73bf8
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2024-02-07 22:28:32 -0700

    Update cr.js

--- cr.js
+++ cr.js
@@ -1,11 +1,11 @@
 function jborgarts(){
 
-    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/branding/JB_square.png'
+    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/branding/borg_d.svg'
     document.getElementById("jborgarts_link").href = 'https://jborgarts.com/'
 }
 
 function jborgartslight(){
 
-    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/branding/JB_square.png'
+    document.getElementById("jborgarts").src = 'https://jborgarts.com/assets/img/branding/borg.svg'
     document.getElementById("jborgarts_link").href = 'https://jborgarts.com/'
 }
