f953f8f5e1eeff2873d1dbb7de9862742b3c326f
Jacob Borg <48691704+JacobBorgProgramming@users.noreply.github.com>
2025-09-27 13:57:29 -0700

    began new site

---
+++ dev/mercury.js
@@ -0,0 +1,8 @@
+function showcontact() {
+  var x = document.getElementById("contact");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+} 
